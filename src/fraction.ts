e4bd05554043d4658ebda81c15be7c8b38d6915a
soheyHashimoto <heyso1209@gmail.com>
2022-08-18 21:59:52 +0900

    feat: add Fraction

---
+++ src/fraction.ts
@@ -0,0 +1,24 @@
+interface fraction {
+  numerator: number;
+  denominator: number;
+  add(...arg: fraction[]): fraction;
+};
+
+export class Fraction implements fraction {
+  numerator: number;
+  denominator: number;
+
+  constructor(numerator: number, denominator: number) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+
+  add(...arg: fraction[]): fraction {
+    const numerators = arg.map((f) => f.numerator);
+    const denominators = arg.map((f) => f.denominator);
+    const numerator = numerators.reduce((a, b) => a + b);
+    const denominator = denominators.reduce((a, b) => a * b);
+
+    return new Fraction(numerator, denominator);
+  }
+}
