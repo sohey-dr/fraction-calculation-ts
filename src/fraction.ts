1266db98ad4fc564aba503e97abedc4358c6505a
soheyHashimoto <heyso1209@gmail.com>
2022-08-18 22:01:19 +0900

    feat: Four arithmetic operations func

--- src/fraction.ts
+++ src/fraction.ts
@@ -21,4 +21,31 @@ export class Fraction implements fraction {
 
     return new Fraction(numerator, denominator);
   }
+
+  sub(...arg: fraction[]): fraction {
+    const numerators = arg.map((f) => f.numerator);
+    const denominators = arg.map((f) => f.denominator);
+    const numerator = numerators.reduce((a, b) => a - b);
+    const denominator = denominators.reduce((a, b) => a * b);
+
+    return new Fraction(numerator, denominator);
+  }
+
+  mul(...arg: fraction[]): fraction {
+    const numerators = arg.map((f) => f.numerator);
+    const denominators = arg.map((f) => f.denominator);
+    const numerator = numerators.reduce((a, b) => a * b);
+    const denominator = denominators.reduce((a, b) => a * b);
+
+    return new Fraction(numerator, denominator);
+  }
+
+  div(...arg: fraction[]): fraction {
+    const numerators = arg.map((f) => f.numerator);
+    const denominators = arg.map((f) => f.denominator);
+    const numerator = numerators.reduce((a, b) => a * b);
+    const denominator = denominators.reduce((a, b) => a * b);
+
+    return new Fraction(numerator, denominator);
+  }
 }
